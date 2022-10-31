112a39a3d494e8b4e6a49d784493a787ff6b9810
sangkips <sangkipkoech@gmail.com>
2022-10-31 12:55:21 +0300

    remove redundant field in serializer

--- apiendpoint/urls.py
+++ apiendpoint/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views
 
-from .views import BioViewSet
+from .views import django_models_json
 
 router = routers.DefaultRouter()
-router.register(r'bio', BioViewSet)
+router.register(r'bio', django_models_json)
 
 
 urlpatterns = [
--- apiendpoint/views.py
+++ apiendpoint/views.py
@@ -1,3 +1,4 @@
+'''
 from django.shortcuts import render
 from rest_framework import status, viewsets
 from .serializers import BioSerializer
@@ -10,3 +11,13 @@ import json
 class BioViewSet(viewsets.ModelViewSet):
     queryset = Bio.objects.all()
     serializer_class = BioSerializer
+
+'''
+from django.core.serializers import serialize
+from django.http import HttpResponse
+from .models import Bio
+
+def django_models_json(request):
+    obj = Bio.objects.first()
+    data = serialize("json", fields=('slackUsername', 'backend', 'age', 'bio'))
+    return HttpResponse(data, content_type="application/json")
