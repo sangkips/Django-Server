21062069ceb3ba945ae95a75b7527ea7ab927a96
sangkips <sangkipkoech@gmail.com>
2022-10-31 10:32:53 +0300

    added json response

--- apiendpoint/views.py
+++ apiendpoint/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from rest_framework import status, viewsets
 from .serializers import BioSerializer
 from .models import Bio
+from rest_framework.response import Response
 
 
 # Create your views here.
 class BioViewSet(viewsets.ModelViewSet):
     queryset = Bio.objects.all()
     serializer_class = BioSerializer
+
+
+    def list(self, request):
+        response = super().list(request)
+        return Response({"success": 1 if response.data else 0, "data": response.data})
