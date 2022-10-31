68edc34677c02890eab9c54af55c98cebe12b344
sangkips <sangkipkoech@gmail.com>
2022-10-31 14:52:30 +0300

    changed api routes

--- apiendpoint/views.py
+++ apiendpoint/views.py
@@ -1,5 +1,6 @@
+from operator import truediv
 from django.http import JsonResponse
 
 def home(request):
     
-    return JsonResponse({"slackUsername": "Sang", "backend": true, "age": 65, "bio": "I am a backend engineer"})
+    return JsonResponse({"slackUsername": "Sang", "backend": truediv, "age": 65, "bio": "I am a backend engineer"})
