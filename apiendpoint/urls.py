9318e5e40a41a93dcb72d99934c0bf4f3188afe8
sangkips <sangkipkoech@gmail.com>
2022-10-31 13:43:43 +0300

    added basename

--- apiendpoint/urls.py
+++ apiendpoint/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from apiendpoint.models import Bio
+
 from .views import django_models_json
 
 router = routers.DefaultRouter()
-router.register(r'bio', django_models_json)
+router.register(r'bio', django_models_json, basename='bio')
 
 
 urlpatterns = [
