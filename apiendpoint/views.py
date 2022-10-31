b42de8795582d21af20e3d6d968273d289133c6b
sangkips <sangkipkoech@gmail.com>
2022-10-31 13:29:00 +0300

    remove redundant field in serializer

--- apiendpoint/views.py
+++ apiendpoint/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 from .models import Bio
 
 def django_models_json(request):
-    obj = Bio.objects.all()
+    queryset = Bio.objects.all()
     data = serialize("json", fields=('slackUsername', 'backend', 'age', 'bio'))
     return HttpResponse(data, content_type="application/json")
