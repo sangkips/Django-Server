c719b88bd65dcedf3dd1ee3a0b3fda1686316b20
sangkips <sangkipkoech@gmail.com>
2022-10-31 15:03:10 +0300

    changed api routes

--- apiendpoint/urls.py
+++ apiendpoint/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-
-from apiendpoint.models import Bio
-
 from . import views
-
 
 urlpatterns = [
     path('', views.home)
--- apiendpoint/views.py
+++ apiendpoint/views.py
@@ -1,6 +1,5 @@
-from operator import truediv
 from django.http import JsonResponse
 
 def home(request):
     
-    return JsonResponse({"slackUsername": "Sang", "backend": truediv, "age": 65, "bio": "I am a backend engineer"})
+    return JsonResponse({"slackUsername": "Sang", "backend": True, "age": 65, "bio": "I am a backend engineer"})
