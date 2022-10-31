eb02ff5f7cb90edfbd6c31eefd2f9c2c8b666ddc
sangkips <sangkipkoech@gmail.com>
2022-10-31 14:51:20 +0300

    changed api routes

--- apiendpoint/views.py
+++ apiendpoint/views.py
@@ -1,10 +1,5 @@
 from django.http import JsonResponse
 
-from apiendpoint.models import Bio
-
 def home(request):
-    queryset = Bio.objects.all()
-    context = {
-        'queryset': queryset
-    }
-    return JsonResponse(context, safe=False)
+    
+    return JsonResponse({"slackUsername": "Sang", "backend": true, "age": 65, "bio": "I am a backend engineer"})
